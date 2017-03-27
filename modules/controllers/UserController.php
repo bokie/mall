507c94630f7d7c74bd0cd55dd2724391fadab061
bokie <yyybokie@gmail.com>
2017-03-27 21:52:41 +0800

    update

---
+++ modules/controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\modules\controllers;
+
+use yii\web\Controller;
+
+class UserController extends Controller
+{
+    public function actionUsers()
+    {
+
+    }
+
+    public function actionReg()
+    {
+
+    }
+}
